885500a7ac58c4703466a4517a4e0521d7788072
Gregor Vollmer <git@dynamic-noise.net>
2016-07-27 10:53:42 +0200

    Very basic processor, reads in REF hits

---
+++ include/reftrackalignprocessor.h
@@ -0,0 +1,66 @@
+#ifndef RefTrackAlignProcessor_h
+#define RefTrackAlignProcessor_h 1
+
+#include "marlin/Processor.h"
+#include "lcio.h"
+#include <string>
+#include <Eigen/Dense>
+
+using namespace lcio;
+using namespace marlin;
+
+/**  
+ * @param CollectionName Name of the MCParticle collection
+ * 
+ * @author F. Gaede, DESY
+ * @version $Id: RefTrackAlignProcessor.h,v 1.4 2005-10-11 12:57:39 gaede Exp $ 
+ */
+
+class RefTrackAlignProcessor : public Processor
+{ 
+public:
+  
+	virtual Processor*  newProcessor() { return new RefTrackAlignProcessor; }
+	
+	
+	RefTrackAlignProcessor();
+	
+	/** Called at the begin of the job before anything is read.
+	 * Use to initialize the processor, e.g. book histograms.
+	 */
+	virtual void init();
+	
+	/** Called for every run.
+	 */
+	virtual void processRunHeader( LCRunHeader* run );
+	
+	/** Called for every event - the working horse.
+	 */
+	virtual void processEvent( LCEvent * evt ); 
+	
+	
+	virtual void check( LCEvent * evt ); 
+	
+	
+	/** Called after data processing for clean up.
+	 */
+	virtual void end();
+  
+  
+protected:
+
+	/// REF tracker data collection name
+	std::string _colRefData;
+	/// fitted Track collection name
+	std::string _colTracks;
+	/// REF alignment DB name
+	std::string _refAlignDB;
+private:
+	std::vector<Eigen::Vector2d> getRefHits(LCCollection* col);
+	std::vector<Eigen::Vector3d> transformRefHits(std::vector<Eigen::Vector2d> hits);
+};
+
+#endif
+
+
+
